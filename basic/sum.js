47785e5ce42786b0c096abd1772955f9eee43582
AnnLinLin <annann0311@hotmail.com.tw>
2021-07-31 13:15:15 +0800

    增加1+2+........n回傳結果

--- basic/sum.js
+++ basic/sum.js
@@ -1,1 +1,13 @@
-console.log("Hello~")
+// console.log("Hello~")
+
+function getSum(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(getSum(2));
+console.log(getSum(5));
+console.log(getSum(10));
