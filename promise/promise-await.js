b9f4c2c3bc4efc8409d5b09bee411e03b3cf6c43
AnnLinLin <annann0311@hotmail.com.tw>
2021-08-06 20:37:33 +0800

    新增promise-await內容

---
+++ promise/promise-await.js
@@ -0,0 +1,31 @@
+let doWork = function (job, timer, isOK) {
+  // 物件： new Promise();
+  // 建構式一定要傳入一個函式，而且這個函式本身會有兩個參數
+  // resolve, reject
+  return new Promise((resolve, reject) => {
+    console.log("in promise");
+    setTimeout(() => {
+      let dt = new Date();
+      if (isOK) {
+        resolve(`完成工作:${job} at${dt.toISOString()}`);
+      } else {
+        reject(`${job}失敗了`);
+      }
+    }, timer);
+  });
+};
+
+let dt = new Date();
+console.log(`開始工作 at ${dt.toISOString()}`);
+
+//await 一定要在 async 的函式裡面用
+async function doAllWorks() {
+  let job1 = await doWork("刷牙", 3000, false);
+  console.log(job1);
+  let job2 = await doWork("吃早餐", 4000, true);
+  console.log(job2);
+  let job3 = await doWork("寫作業", 5000, true);
+  console.log(job3);
+}
+
+doAllWorks();
---
+++ promise/promise-then.js
@@ -0,0 +1,42 @@
+let doWork = function (job, timer, isOK) {
+  // 物件： new Promise();
+  // 建構式一定要傳入一個函式，而且這個函式本身會有兩個參數
+  // resolve, reject
+  return new Promise((resolve, reject) => {
+    console.log("in promise");
+    setTimeout(() => {
+      let dt = new Date();
+      if (isOK) {
+        resolve(`完成工作:${job} at${dt.toISOString()}`);
+      } else {
+        reject(`${job}失敗了`);
+      }
+    }, timer);
+  });
+};
+
+let dt = new Date();
+console.log(`開始工作 at ${dt.toISOString()}`);
+
+let job1 = doWork("刷牙", 3000, true);
+job1
+  .then((result) => {
+    console.log("first", result);
+    return doWork("吃早餐", 3000, true);
+  })
+  .then((result) => {
+    console.log("second", result);
+    return doWork("寫作業", 5000, true);
+  })
+  .then((result) => {
+    console.log("third", result);
+    return doWork("寫作業", 5000, true);
+  })
+  .catch((error) => {
+    // 捕捉catch前面所有錯誤
+    console.log(error);
+  })
+  .finally(() => {
+    // 無論成功或失敗都會在這裡
+    console.log("finally");
+  });
