107f1c941370d2b32e96837fdc5aa7ae32a2e896
AnnLinLin <annann0311@hotmail.com.tw>
2021-08-06 20:24:32 +0800

    新增promise-then.js內容

--- promise/promise1.js
+++ promise/promise1.js
@@ -18,13 +18,33 @@ let doWork = function (job, timer, isOK) {
 let dt = new Date();
 console.log(`開始工作 at ${dt.toISOString()}`);
 
-let job1 = doWork("刷牙", 3000, false);
-console.log(job1);
-job1.then(
-  function (resolve) {
-    console.log("第 1 個函式被呼叫了", resolve);
-  },
-  function (reject) {
-    console.log("第 2 個函式被呼叫了", reject);
-  }
-);
+// let job1 = doWork("刷牙", 3000, false);
+// console.log(job1);
+// job1.then(
+//   function (resolve) {
+//     console.log("第 1 個函式被呼叫了", resolve);
+//   },
+//   function (reject) {
+//     console.log("第 2 個函式被呼叫了", reject);
+//   }
+// );
+
+let job1 = doWork("刷牙", 3000, true);
+job1
+  .then((result) => {
+    console.log("第 1 個函式被呼叫了", result);
+    return 1;
+    // 即使我們回傳的是數字，還是會包成 promise 物件
+    // Promise.resolve(1)
+  })
+  .then((result) => {
+    console.log("第 2 個 then", result);
+  })
+  .catch((error) => {
+    // 捕捉錯誤
+    console.log("第 2 個函式被呼叫了", error);
+  })
+  .finally(() => {
+    // 無論成功或失敗都會在這裡
+    console.log("finally");
+  });
