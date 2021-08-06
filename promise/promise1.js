7db9237ff96d86e634395ab0133a6d96e84a10bd
AnnLinLin <annann0311@hotmail.com.tw>
2021-08-06 13:07:18 +0800

    add promise1.js

---
+++ promise/promise1.js
@@ -0,0 +1,27 @@
+let doWork = function (job, timer, isOK) {
+  return new Promise((resolve, reject) => {
+    console.log("in promise");
+    setTimeout(() => {
+      let dt = new Date();
+      if (isOK) {
+        resolve(`完成工作:${job} at${dt.toISOString()}`);
+      } else {
+        reject(`${job}失敗了`);
+      }
+    }, timer);
+  });
+};
+
+let dt = new Date();
+console.log(`開始工作 at ${dt.toISOString()}`);
+
+let job1 = doWork("刷牙", 3000, false);
+console.log(job1);
+job1.then(
+  function (resolve) {
+    console.log("第 1 個函式被呼叫了", resolve);
+  },
+  function (reject) {
+    console.log("第 2 個函式被呼叫了", reject);
+  }
+);
