84b787f8c40ea44ca22e1fd0667ca7afe7d0186d
AnnLinLin <annann0311@hotmail.com.tw>
2021-08-07 13:16:22 +0800

    finish quiz2.js

--- promise/quiz2.js
+++ promise/quiz2.js
@@ -14,9 +14,12 @@ asyncF();
 console.log(5);
 
 // 4 ->1 ->5 ->2 ->3
-//執行 L12 (印出4)
-//->執行 L13 (印出1)
-// setTimeout丟給暗樁
-//->執行L14(印出5)
-//stack已清空
-//->(印出2跟3) async await 會依序執行
+//->執行 L12 (印出4)
+//->執行 L13 呼叫asyncF()
+//->執行 L2  (印出1)
+// setTimeout丟給暗樁 (setTimeout執行完會丟到queue中等待stack清空)
+//->執行 L14 (印出5)
+//stack已清空 將queue等待中的東西搬進stack
+//->執行 L5  (印出2) 
+//->執行 L9  (印出3)  async await 會依序執行
+
